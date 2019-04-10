64c04e2a51b90a3e21acd1c90abdead9279f956b
doraemon9001 <doraemon9001@hotmail.com>
2019-04-10 20:36:56 +0800

    使用字符串模板

--- app/src/main/java/com/lance/guess/MainActivity.kt
+++ app/src/main/java/com/lance/guess/MainActivity.kt
@@ -2,12 +2,21 @@ package com.lance.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val word = "MainActivity OnCreact"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val txtShow = findViewById<TextView>(R.id.txtShow)
+        txtShow.setText("test: ${word}")
+
     }
+
+
+
 }
