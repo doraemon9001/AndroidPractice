5b962dbdd94521f1aa8f88da23023ff6cf699cc3
doraemon9001 <doraemon9001@hotmail.com>
2019-04-11 20:14:45 +0800

    1. SharedPreferenct 賦值及取值

--- app/src/main/java/com/lance/guess/ResultActivity.kt
+++ app/src/main/java/com/lance/guess/ResultActivity.kt
@@ -1,7 +1,9 @@
 package com.lance.guess
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
@@ -13,5 +15,15 @@ class ResultActivity : AppCompatActivity() {
         val resultText = intent.getStringExtra("result")
 
         tv_result.text = resultText
+
+        //. SharedPreferences賦值
+        getSharedPreferences("GUESS", Context.MODE_PRIVATE)
+            .edit()
+            .putString("REC_RESULT", resultText)
+            .apply()
+        //. SharedPreferences取值
+        Log.d("REC_RESULT", getSharedPreferences("GUESS", Context.MODE_PRIVATE)
+            .getString("REC_RESULT", ""))
+
     }
 }
